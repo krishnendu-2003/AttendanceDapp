92a8a882075f816ec577f1be8787d5183f2bd6ef
krishnendu-2003 <skrishnendu115@gmail.com>
2024-10-22 16:03:07 +0530

    Updated bavbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { gsap } from 'gsap';
-import './Navbar.css'; // Import the CSS file
+import './Navbar.css';
 
 const Navbar = () => {
   const [account, setAccount] = useState(null);
